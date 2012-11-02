ab287c600be85dae0207f9e24a3f01b358de9b16
run4kelly <r2624kel@comcast.net>
2012-11-02 15:28:08 -0600

    make a basic user model including secure passwords

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
+# add a new comment line
 
 class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
