3f909df240bfd5d785ba45c4dad7a2267f26663c
run4kelly <r2624kel@comcast.net>
2012-11-05 15:28:55 -0700

    Code cleanup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,16 +5,30 @@ class UsersController < ApplicationController
   end
 
   def new
-    @user = User.new
+     @user = User.new
   end
 
   def create
     @user = User.new(params[:user])
     if @user.save
+      sign_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
       render 'new'
+    end  #end for the if
+  end  #end for the def create
+
+  def edit
+  end
+
+  def update
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
     end
   end
-end
+end  # end for the class
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -4,8 +4,14 @@ describe "Authentication" do
 
   subject { page }
 
-  describe "signin" do
+  describe "signin page" do
+    before { visit signin_path }
 
+    it { should have_selector('h1',    text: 'Sign in') }
+    it { should have_selector('title', text: 'Sign in') }
+  end
+
+  describe "signin" do
     before { visit signin_path }
 
     describe "with invalid information" do
@@ -18,8 +24,8 @@ describe "Authentication" do
         before { click_link "Home" }
         it { should_not have_selector('div.alert.alert-error') }
       end
-     end
-
+    end
+    
     describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
       before do
@@ -29,9 +35,17 @@ describe "Authentication" do
       end
 
       it { should have_selector('title', text: user.name) }
+
+      it { should have_link('Users',    href: users_path) }
       it { should have_link('Profile', href: user_path(user)) }
+      it { should have_link('Settings', href: edit_user_path(user)) }
       it { should have_link('Sign out', href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
-     end
+
+      describe "followed by signout" do
+        before { click_link "Sign out" }
+        it { should have_link('Sign in') }
+      end
+    end
   end
 end
