f03ed1f78ce2008f1d970d5d15c2c6ba57f673e3
run4kelly <r2624kel@comcast.net>
2012-11-08 09:26:08 -0700

    Add user following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_filter :signed_in_user, 
+                only: [:index, :edit, :update, :destroy, :following, :followers]
   before_filter :correct_user,   only: [:edit, :update]
   before_filter :admin_user,     only: :destroy
 
@@ -13,7 +14,7 @@ class UsersController < ApplicationController
   end
 
   def new
-     @user = User.new
+    @user = User.new
   end
 
   def create
@@ -24,12 +25,12 @@ class UsersController < ApplicationController
       redirect_to @user
     else
       render 'new'
-    end  #end for the if
-  end  #end for the def create
+    end
+  end
 
   def edit
   end
-    
+
   def update
     if @user.update_attributes(params[:user])
       flash[:success] = "Profile updated"
@@ -39,25 +40,35 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
-  
+
   def destroy
     User.find(params[:id]).destroy
-    flash[:success] = "User destroyed"
-    redirect_to users_path
+    flash[:success] = "User destroyed."
+    redirect_to users_url
+  end
+
+  def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.followed_users.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
   end
 
   private
 
-    def signed_in_user
-      redirect_to signin_url, notice: "Please sign in." unless signed_in?
-    end
-
     def correct_user
       @user = User.find(params[:id])
-      redirect_to(root_path) unless current_user?(@user)
+      redirect_to(root_url) unless current_user?(@user)
     end
 
     def admin_user
-      redirect_to(root_path) unless current_user.admin?
+      redirect_to(root_url) unless current_user.admin?
     end
-end  # end for the class
+end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -5,29 +5,54 @@ describe "User pages" do
   subject { page }
 
   describe "index" do
-
-    let(:user) { FactoryGirl.create(:user) }
-
-    before(:all) { 30.times { FactoryGirl.create(:user) } }
-    after(:all)  { User.delete_all }
-
-    before(:each) do
-      sign_in user
+    
+    before do
+      sign_in FactoryGirl.create(:user)
+      FactoryGirl.create(:user, name: "Bob", email: "bob@example.com")
+      FactoryGirl.create(:user, name: "Ben", email: "ben@example.com")
       visit users_path
     end
 
     it { should have_selector('title', text: 'All users') }
-    it { should have_selector('h1',    text: 'All users') }
-
 
     describe "pagination" do
-      it { should have_selector('div.pagination') }
+      
+      before(:all) { 30.times { FactoryGirl.create(:user) } }
+      after(:all)  { User.delete_all }
+
+      let(:first_page)  { User.paginate(page: 1) }
+      let(:second_page) { User.paginate(page: 2) }
+
+      it { should have_link('Next') }
+      its(:html) { should match('>2</a>') }
 
       it "should list each user" do
-        User.paginate(page: 1).each do |user|
-          page.should have_selector('li>a', text: user.name)
+        User.all[0..2].each do |user|
+          page.should have_selector('li', text: user.name)
         end
-      end      
+      end
+
+      it "should list the first page of users" do
+        first_page.each do |user|
+          page.should have_selector('li', text: user.name)
+        end
+      end
+
+      it "should not list the second page of users" do
+        second_page.each do |user|
+          page.should_not have_selector('li', text: user.name)
+        end
+      end
+
+      describe "showing the second page" do
+        before { visit users_path(page: 2) }
+
+        it "should list the second page of users" do
+          second_page.each do |user|
+            page.should have_selector('li', text: user.name)
+          end
+        end
+      end 
     end
 
     describe "delete links" do
@@ -50,13 +75,6 @@ describe "User pages" do
     end
   end
 
-  describe "signup page" do
-    before { visit signup_path }
-
-    it { should have_selector('h1',    text: 'Sign up') }
-    it { should have_selector('title', text: full_title('Sign up')) }
-  end
-
   describe "profile page" do
     let(:user) { FactoryGirl.create(:user) }
     let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo") }
@@ -65,7 +83,7 @@ describe "User pages" do
     before { visit user_path(user) }
 
     it { should have_selector('h1',    text: user.name) }
-    it { should have_selector('title', text: user.name) }  
+    it { should have_selector('title', text: user.name) }
 
     describe "microposts" do
       it { should have_content(m1.content) }
@@ -73,11 +91,8 @@ describe "User pages" do
       it { should have_content(user.microposts.count) }
     end
 
-    describe "microposts" do
-      it { should have_content(m1.content) }
-      it { should have_content(m2.content) }
-      it { should have_content(user.microposts.count) }
-    end
+    it { should have_selector('h1',    text: user.name) }
+    it { should have_selector('title', text: user.name) }
 
     describe "follow/unfollow buttons" do
       let(:other_user) { FactoryGirl.create(:user) }
@@ -130,8 +145,15 @@ describe "User pages" do
     end
   end
 
+  describe "signup page" do
+    before { visit signup_path }
+
+    it { should have_selector('h1',    text: 'Sign up') }
+    it { should have_selector('title', text: full_title('Sign up')) }
+  end
+
   describe "signup" do
-    
+
     before { visit signup_path }
 
     let(:submit) { "Create my account" }
@@ -140,33 +162,31 @@ describe "User pages" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
-
-      describe "after submission" do
+      
+      describe "error messages" do
         before { click_button submit }
 
         it { should have_selector('title', text: 'Sign up') }
         it { should have_content('error') }
-        it { should_not have_content('Password digest') }
       end
     end
 
     describe "with valid information" do
-
       before do
         fill_in "Name",         with: "Example User"
         fill_in "Email",        with: "user@example.com"
         fill_in "Password",     with: "foobar"
-        fill_in "Confirmation", with: "foobar"        
+        fill_in "Confirmation", with: "foobar"
       end
-      
+
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
 
-      describe "after saving a user" do
+      describe "after saving the user" do
         before { click_button submit }
-
-        let(:user) { User.find_by_email("user@example.com") }
+        
+        let(:user) { User.find_by_email('user@example.com') }
 
         it { should have_selector('title', text: user.name) }
         it { should have_selector('div.alert.alert-success', text: 'Welcome') }
@@ -174,7 +194,7 @@ describe "User pages" do
       end
     end
   end
-
+  
   describe "edit" do
     let(:user) { FactoryGirl.create(:user) }
     before do
@@ -183,7 +203,6 @@ describe "User pages" do
     end
 
     describe "page" do
-
       it { should have_selector('h1',    text: "Update your profile") }
       it { should have_selector('title', text: "Edit user") }
       it { should have_link('change', href: 'http://gravatar.com/emails') }
@@ -196,7 +215,7 @@ describe "User pages" do
     end
 
     describe "with valid information" do
-      let(:new_name) { "New Name" }
+      let(:new_name)  { "New Name" }
       let(:new_email) { "new@example.com" }
       before do
         fill_in "Name",             with: new_name
@@ -207,8 +226,8 @@ describe "User pages" do
       end
 
       it { should have_selector('title', text: new_name) }
-      it { should have_link('Sign out', href: signout_path) }
       it { should have_selector('div.alert.alert-success') }
+      it { should have_link('Sign out', href: signout_path) }
       specify { user.reload.name.should  == new_name }
       specify { user.reload.email.should == new_email }
     end
@@ -219,7 +238,7 @@ describe "User pages" do
     let(:other_user) { FactoryGirl.create(:user) }
     before { user.follow!(other_user) }
 
-    describe "followed users (following)" do
+    describe "followed users" do
       before do
         sign_in user
         visit following_user_path(user)
